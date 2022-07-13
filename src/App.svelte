55b98cdadf0e023955aea138b7f5b66dac78a411
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-13 23:22:27 +0500

    Блок each

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
-    let x = 12
+  let cats = [
+    { id: 'J---aiyznGQ', name: 'Кот на клавишных' },
+    { id: 'z_AbfPXTKms', name: 'Мару' },
+    { id: 'OUtn3pvWmpg', name: 'Экзистенциальный кот' }
+  ];
 </script>
 
-{#if x > 10}
-    <p>{x} больше, чем 10</p>
-{:else if 5 > x}
-    <p>{x} меньше, чем 5</p>
-{:else}
-    <p>{x} между 5 и 10</p>
-{/if}
+<h1>Знаменитые YouTube котики</h1>
+
+<ul>
+    {#each cats as cat, i}
+        <li>{i + 1}) <a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+            {cat.name}
+        </a></li>
+    {/each}
+</ul>
