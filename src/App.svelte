01853a7da584c452a459f05eb0945936128e392e
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-13 23:15:51 +0500

    Развертка свойств

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
 <script>
   import Nested from "./Nested.svelte";
+
+  const pkg = {
+    name: 'svelte',
+    version: 3,
+    speed: 'быстро',
+    website: 'https://svelte.dev'
+  }
 </script>
 
-<Nested />
+<Nested name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website}/>
+<Nested {...pkg}/>
