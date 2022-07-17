f5af10fde7bc7356d975df66b12b64f8548867e5
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-17 23:10:47 +0500

    Выпадающие списки

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,39 @@
 <script>
-    import {marked} from 'marked';
-    let value = `Что-то написано *курсивом*, а что-то **жирным** шрифтом`;
+    let questions = [
+        { id: 1, text: `В какой школе ты учился?` },
+        { id: 2, text: `Девичья фамилия твоей мамы?` },
+        { id: 3, text: `Любая другая персональная информация, которую злоумышленник спокойно нагуглит?` }
+    ];
+
+    let selected;
+
+    let answer = '';
+
+    function handleSubmit() {
+        alert(`Ответ на вопрос №${selected.id} (${selected.text}): "${answer}"`);
+    }
 </script>
 
-{@html marked(value)}
+<h2>Секретные вопросы</h2>
 
-<textarea bind:value />
+<form on:submit|preventDefault={handleSubmit}>
+    <select bind:value={selected} on:change="{() => answer = ''}">
+        {#each questions as question}
+            <option value={question}>
+                {question.text}
+            </option>
+        {/each}
+    </select>
+
+    <input bind:value={answer}>
+
+    <button disabled={!answer} type=submit>
+        Отправить
+    </button>
+</form>
+
+<p>выбранный вопрос {selected ? selected.id : '[ждём...]'}</p>
 
 <style>
-    textarea {
-        width: 100%;
-        height: 200px;
-    }
+    input { display: block; width: 500px; max-width: 100%; }
 </style>
